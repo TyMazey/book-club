2f2175ed63ab2f7e7efbd2b3a6bb50ac7234dedb
vjp888 <rellik888@gmail.com>
2019-01-28 17:49:55 -0700

    Adds name_as_kebab method and test to app
    co-authored-by: 43197720+TyMazey@users.noreply.github.com

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
-  
+  def name_as_kebab
+    title.downcase.gsub(/ /, "-")
+  end
 end
--- spec/books/model/book_spec.rb
+++ spec/books/model/book_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: :model do
+RSpec.describe Book do
   describe 'class methods' do
     describe '.name_as_kebab' do
-      book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish")
+      it '.name_as_kebab' do
+        book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish")
 
-      expect(book_1.name_as_kebab).to eq("mody-dick")
+        expect(book_1.name_as_kebab).to eq("moby-dick")
+      end
     end
   end
 end
