1e6b5a53f39a2d08bfad699e5572aed6577b05c2
vjp888 <rellik888@gmail.com>
2019-01-28 17:49:55 -0700

    Adds feature test for user show page
    co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to:'landing_page#index'
+  root 'landing_page#index'
   resources :books
   resources :authors, only: [:show]
+  resources :user, only: [:show]
 end
