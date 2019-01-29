dd656c4fb7b5e41131ab1167bd0a025693f521d0
vjp888 <rellik888@gmail.com>
2019-01-28 17:49:55 -0700

    Completes show page for user and adjusts test to look within css containers
    co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

--- spec/features/review/user_sees_show_page_for_user_spec.rb
+++ spec/features/review/user_sees_show_page_for_user_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe 'As a visitor', type: :feature do
     visit user_path(user)
 
     expect(page).to have_content(user.name)
+
+    within ".reviews"
     expect(page).to have_content(review.title)
     expect(page).to have_content(review.description)
     expect(page).to have_content(review.rating)
     expect(page).to have_content(book.title)
-    expect(page).to have_content(book.pages)
-    expect(page).to have_content(book.year_published)
     expect(page).to have_xpath("//img[@src='ixat yzarc']")
     expect(page).to have_content(review.created_at)
   end
