9698dd500528e1da7c204fd5087656173519b92e
vjp888 <rellik888@gmail.com>
2019-01-28 17:49:55 -0700

    adds test for top review user class method

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -59,24 +59,57 @@ RSpec.describe Book, type: :model do
     end
   end
 
+
   describe 'class methods' do
-    it '.best_books'
+    it '.best_books' do
       user_1 = User.create(name: "bob")
       book_2 = Book.create(title: "Hitch Hikers: A strange tail", pages: 302, year_published: 1940, thumbnail: "harby")
       book_4 = Book.create(title: "Mortal Enemies", pages: 400, year_published: 1970, thumbnail: "friendly")
       book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish")
       book_3 = Book.create(title: "Colin's Odd Trip", pages: 20, year_published: 2100, thumbnail: "strange")
       book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
-      book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
-      book_2.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
-      book_3.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
-      book_4.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
+      book_1.reviews.create(title: "Amazing", description: "Really", rating: 4, user: user_1)
+      book_2.reviews.create(title: "Amazing", description: "Really", rating: 4, user: user_1)
+      book_3.reviews.create(title: "Amazing", description: "Really", rating: 3, user: user_1)
+      book_4.reviews.create(title: "Amazing", description: "Really", rating: 1, user: user_1)
 
       top_3 = [book_1, book_2, book_3]
 
       expect(Book.best_books).to eq(top_3)
     end
-    it '.worst_books'
 
-    it '.top_review_users'
+    it '.worst_books' do
+      user_1 = User.create(name: "bob")
+      book_2 = Book.create(title: "Hitch Hikers: A strange tail", pages: 302, year_published: 1940, thumbnail: "harby")
+      book_4 = Book.create(title: "Mortal Enemies", pages: 400, year_published: 1970, thumbnail: "friendly")
+      book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish")
+      book_3 = Book.create(title: "Colin's Odd Trip", pages: 20, year_published: 2100, thumbnail: "strange")
+      book_2.reviews.create(title: "Amazing", description: "Really", rating: 4, user: user_1)
+      book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)
+      book_3.reviews.create(title: "Amazing", description: "Really", rating: 3, user: user_1)
+      book_4.reviews.create(title: "Amazing", description: "Really", rating: 1, user: user_1)
+      book_1.reviews.create(title: "Amazing", description: "Really", rating: 4, user: user_1)
+
+      worst_3 = [book_4, book_3, book_2]
+
+      expect(Book.worst_books).to eq(worst_3)
+    end
+
+    it '.top_review_users' do
+      author = Author.create(name: "Rickey Bobby")
+      book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish", authors: [author])
+      user_3 = User.create(name: "tod")
+      user_4 = User.create(name: "steve")
+      user_2 = User.create(name: "rob")
+      user_1 = User.create(name: "bob")
+      5.times {book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_1)}
+      4.times {book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_2)}
+      3.times {book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_3)}
+      2.times {book_1.reviews.create(title: "Amazing", description: "Really", rating: 5, user: user_4)}
+
+      top_reviewers = [user_1, user_2, user_3]
+
+      expect(Book.top_review_users).to eq(top_reviewers)
+    end
+  end
 end
