8df3eb4f3b5ee4853f0c4356d99bf35ba593d90f
Ty Mazey <tymazey@gmail.com>
2019-01-30 15:34:56 -0700

    adds a tets for a author intance method that removes its own name from books
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Author, type: :model do
 
     it {should validate_presence_of :name}
   end
+
+  describe 'instance methods' do
+
+    describe '.remove_self_from_books' do
+      it 'should remove its own name from books as author' do
+        author = Author.create(name: "Baby Jesus")
+        author_2 = Author.create(name: "Rickey Bobby")
+        book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish", authors: [author, author_2])
+        new_book = author.remove_self_from_books.first.authors
+
+
+        expect(new_book).to eq(["Rickey Bobby"])
+      end
+    end
+  end
 end
