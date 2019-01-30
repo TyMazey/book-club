ffa3271d7f31696946ad8e6aa32b940d79070bde
Ty Mazey <tymazey@gmail.com>
2019-01-29 17:44:24 -0700

    removes name as kebab test
    -authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -16,12 +16,5 @@ RSpec.describe Book, type: :model do
   end
 
   describe 'instance methods' do
-    describe '.name_as_kebab' do
-      it '.name_as_kebab' do
-        book_1 = Book.create(title: "Moby Dick", pages: 100, year_published: 1900, thumbnail: "gibberish")
-
-        expect(book_1.name_as_kebab).to eq("moby-dick")
-      end
-    end
   end
 end
