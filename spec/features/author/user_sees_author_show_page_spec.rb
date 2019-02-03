1bc6ad84f98b2c41d45f7130368018b48c8e1246
Ty Mazey <tymazey@gmail.com>
2019-02-03 10:58:21 -0700

    creates feature test for authors top review

--- spec/features/author/user_sees_author_show_page_spec.rb
+++ spec/features/author/user_sees_author_show_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'as visitor', type: :feature do
 
-  it 'shows me information about an author' do
+  it 'shows information about an author' do
     author = Author.create(name: "bob")
     book_1 = author.books.create(title: "book 1", pages: 100, year_published: 1901, thumbnail: "picture url")
     book_2 = author.books.create(title: "book_1", pages: 200, year_published:1702, thumbnail: "picture url")
@@ -39,5 +39,23 @@ RSpec.describe 'as visitor', type: :feature do
     expect(page).to have_content(book_1.year_published)
 
     expect(page).to_not have_content(book_2.title)
+  end
+
+  it 'shows the top review for the author' do
+    author = Author.create(name: "bob")
+    book = author.books.create(title: "book 1", pages: 100, year_published: 1901, thumbnail: "picture url")
+    user = User.create(name: 'Bob')
+    user_1 = User.create(name: 'Jeef')
+    book.reviews.create(title: 'great', description: 'great', rating: 5, user: user)
+    book.reviews.create(title: 'bad', description: 'bad', rating: 1, user: user_1)
+
+    visit author_path(author.id)
+
+    within '.review' do
+      expect(page).to have_content('Great')
+      expect(page).to have_content('Bob')
+      expect(page).to_not have_content('Bad')
+      expect(page).to_not have_content('Jeef')
+    end
   end
 end
