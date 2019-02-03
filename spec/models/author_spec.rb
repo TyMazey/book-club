265b247012276c6aa81cb21e110a2bb7cea45a61
Ty Mazey <tymazey@gmail.com>
2019-02-03 11:12:04 -0700

    makes model test for best_review  method on an author

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Author, type: :model do
 
     it {should validate_presence_of :name}
   end
+
+  describe 'instance methods' do
+    describe '.best_review' do
+      it 'returns the top review for a specific author' do
+        author = Author.create(name: "bob")
+        book = author.books.create(title: "book 1", pages: 100, year_published: 1901, thumbnail: "picture url")
+        user = User.create(name: 'Bob')
+        user_1 = User.create(name: 'Jeef')
+        good = book.reviews.create(title: 'great', description: 'great', rating: 5, user: user)
+        bad = book.reviews.create(title: 'bad', description: 'bad', rating: 1, user: user_1)
+
+        expect(author.best_review).to eq([good])
+      end
+    end
+  end
 end
