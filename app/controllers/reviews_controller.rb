2c8eea45a631d50f0348cad22e225c1ae27a62c9
Ty Mazey <tymazey@gmail.com>
2019-02-03 10:43:13 -0700

    updates reviews controller to save review on book, and redirects to the books show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,23 @@
 class ReviewsController < ApplicationController
 
   def new
+    book = Book.find(params[:book_id])
+    @review = book.reviews.new
+    @book = params[:book_id]
+  end
 
+  def create
+    user = User.find_or_create_by(name: params[:review][:user].titlecase)
+    book = Book.find(params[:book_id].titlecase)
+    @review = book.reviews.new(review_params)
+    @review.update(user: user)
+    @review.save
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :description, :rating)
   end
 end
