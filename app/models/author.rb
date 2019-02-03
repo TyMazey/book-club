48caf4319923b59ea82de9a65c0ad372b143761f
Ty Mazey <tymazey@gmail.com>
2019-02-03 11:21:16 -0700

    adds best_review method to author model and passes test

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,8 @@ class Author < ApplicationRecord
   has_many :books, through: :author_books
 
   validates_presence_of :name
+
+  def best_review
+    Review.joins(:book).order(rating: :desc).limit(1)
+  end
 end
